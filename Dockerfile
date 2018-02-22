08390ae2839e580e5e0250b4f4964ca9f26d630c
tvalerio <tiidle@gmail.com>
2018-02-22 11:41:13 -0300

    Correções e dockerfile

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM openjdk:8
+ADD target/system2.jar /opt/system2/
+WORKDIR "/opt/system2/"
+CMD ["java","-jar","system2.jar"]
+EXPOSE 8080
--- src/system2/service.clj
+++ src/system2/service.clj
@@ -22,7 +22,7 @@
               ;::http/allowed-origins ["http://localhost:3000"]
               ::http/resource-path "/public"
               ::http/type :jetty
-              ::http/port 8081
+              ::http/port 8080
               ::http/container-options {:h2c? true
                                         :h2? false
                                         :ssl? false}})
